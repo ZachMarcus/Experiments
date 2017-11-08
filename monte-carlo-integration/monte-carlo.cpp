23b6ab40521ec276734ad19dd99fb2b3d7f05dd0
ZachMarcus <zachary.j.marcus@gmail.com>
2017-11-08 12:28:59 -0500

    Updated monte-carlo approximation of pi program to work

--- monte-carlo-integration/monte-carlo.cpp
+++ monte-carlo-integration/monte-carlo.cpp
@@ -1,9 +1,15 @@
-
-#include <iostream>
+#include <cmath>
 #include <cstdlib>
+#include <iomanip>
+#include <iostream>
 #include <mpi.h>
+#include <random>
 
 
+// C++ program to approximate pi using the Monte Carlo method
+// Distributes work across processes using MPI
+// Makes use of mersenne twister random number generator
+// Uses MPI based reduction to gather results back from processes
 int main(int argc, char** argv) {
   if (argc != 2) {
     std::cerr << "Usage: ./monte-carlo [numIterations]" << std::endl;
@@ -11,34 +17,37 @@ int main(int argc, char** argv) {
   }
 
   int numIterations = atoi(argv[1]);
-  int rank; // to store the relative rank of the running process
-  int world_size;
+  int rank;       // to store the relative rank of the running process
+  int world_size; // to know how many processes are running
 
-  int x, y, z;
-  int processFound;
-  int totalFound;
+  double x, y;
+  int processFound = 0; // to store how many numbers fall in the acceptable
+                        // range in this process
+  int sumFound = 0;   // for the reduction
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &world_size); 
 
-
+  // C++11 random number generation
+  std::random_device randomDevice;
+  std::mt19937 mersenneTwister(randomDevice());
+  std::uniform_real_distribution<> numberDistribution(0,1); 
  
-  if (rank == -1) {} else {
-    for (unsigned int i = 0; i < numIterations; i++) {
-      // do a random number seed
-      x = (double)random()/RAND_MAX;
-      y = (double)random()/RAND_MAX;
-      if (sqrt((x*x)+y*y) <= 1) {
-        processFound++;
-      }
+  for (int i = 0; i < numIterations; i++) {
+    // do a random number seed
+    x = numberDistribution(mersenneTwister);
+    y = numberDistribution(mersenneTwister);
+    if (sqrt((x*x)+(y*y)) <= 1) { // it falls within the montecarlo square
+      processFound++;
     }
   }
 
   // Reduce the computed numbers back down to process rank 0
-  MPI_Reduce(&processFound, &totalFound, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+  MPI_Reduce(&processFound, &sumFound, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
   if (rank == 0) {
-    int pi = (double)totalFound / ((double)numIterations * 4 * world_size);
-    std::cout << "Calculated Pi as " << pi << std::endl;
+    double pi = (4 * (double)sumFound) / 
+               ((double)numIterations * (double)world_size); 
+    std::cout << std::setprecision(8) << "Calculated Pi as " << pi << "\n";
   }
 
   MPI_Finalize();
