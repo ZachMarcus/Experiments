737870f1f21d9ca22e31ae59ca8fefabe5fd8d7e
Zachary Marcus <zmarcus@Theta.coe.neu.edu>
2017-11-09 17:46:44 -0500

    Added more to cache script to get more useful info for assignment

--- matrix-matrix-cachetest/cache.py
+++ matrix-matrix-cachetest/cache.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import subprocess
-
+import math
 
 base_command = "cat /sys/devices/system/cpu/cpu0/cache/index"
 get_L1D_size = base_command + "0/size"
@@ -18,4 +18,37 @@ L3 = subprocess.check_output(get_L3_size.split(" ")).decode("utf8").rstrip()
 print("L1: {}".format(L1))
 print("L2: {}".format(L2))
 print("L3: {}".format(L3))
+
+
+
+if "K" in L1 and "K" in L2 and "K" in L3:
+    L1 = int(L1[:-1]) * 1000
+    L2 = int(L2[:-1]) * 1000
+    L3 = int(L3[:-1]) * 1000
+else:
+    exit()
+
+size_of_int = 4 # bytes
+
+
+L1_elements = int(L1 / size_of_int)
+L2_elements = int(L2 / size_of_int)
+L3_elements = int(L3 / size_of_int)
+
+print("{} int elements allowed in L1".format(L1_elements))
+print("{} int elements allowed in L2".format(L2_elements))
+print("{} int elements allowed in L3".format(L3_elements))
+
+
+L1_matrix_size = math.sqrt(L1_elements)
+L2_matrix_size = math.sqrt(L2_elements)
+L3_matrix_size = math.sqrt(L3_elements)
+
+print("L1 matrix size: {}".format(L1_matrix_size))
+print("L2 matrix size: {}".format(L2_matrix_size))
+print("L3 matrix size: {}".format(L3_matrix_size))
+
+
+
+
 
