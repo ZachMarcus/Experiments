cad01889716fcd53bcd538fa4df89ef81e879cae
Zachary Marcus <zmarcus@Zeta.ece.neu.edu>
2017-11-10 15:06:45 -0500

    Attempting another form of inversion, this time Gauss-Jordan elimination

--- matrix-inversion/matrix-inversion.cpp
+++ matrix-inversion/matrix-inversion.cpp
@@ -29,21 +29,22 @@ void calculateTranspose(T array[MATRIX_SIZE][MATRIX_SIZE], T factors[MATRIX_SIZE
 
 
 template<class T>
-T calculateDeterminant(T array[][MATRIX_SIZE], int size) {
+T calculateDeterminant(T matrix[][MATRIX_SIZE], int size) {
   T temp = 1;
+  std::cout << "Determinant func, size: " << size << std::endl;
   T determinant = 0;
-  T tempArray[MATRIX_SIZE][MATRIX_SIZE];
+  T tempMatrix[MATRIX_SIZE][MATRIX_SIZE];
   if (size == 1) {
-    return array[0][0];
+    return matrix[0][0];
   } else {
     for (int c = 0; c < size; c++) {
       int m = 0;
       int n = 0;
       for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-          tempArray[i][j] = 0;
+          tempMatrix[i][j] = 0;
           if (i != 0 && j!= c) {
-            tempArray[m][n] = array[i][j];
+            tempMatrix[m][n] = matrix[i][j];
             if (n < (size - 2)) {
               n++;
             } else {
@@ -53,7 +54,7 @@ T calculateDeterminant(T array[][MATRIX_SIZE], int size) {
           }
         }
       }
-      determinant = determinant + temp * (array[0][c] * calculateDeterminant(tempArray, size-1));
+      determinant = determinant + temp * (matrix[0][c] * calculateDeterminant(tempMatrix, size-1));
       temp = -1 * temp;
     }
   }
@@ -64,12 +65,14 @@ T calculateDeterminant(T array[][MATRIX_SIZE], int size) {
 template<class T>
 void calculateCofactor(T array[MATRIX_SIZE][MATRIX_SIZE], T factor) {
   T tempArray[MATRIX_SIZE][MATRIX_SIZE], factors[MATRIX_SIZE][MATRIX_SIZE];
+  int m, n;
   for (int q = 0; q < factor; q++) {
     for (int p = 0; p < factor; p++) {
-      int m = 0;
-      int n = 0;
+      m = 0;
+      n = 0;
       for (int i = 0; i < factor; i++) {
         for (int j = 0; j < factor; j++) {
+          tempArray[i][j] = 0;
           if (i != q && j != p) {
             tempArray[m][n] = array[i][j];
             if (n < (factor - 2)) {
@@ -89,21 +92,26 @@ void calculateCofactor(T array[MATRIX_SIZE][MATRIX_SIZE], T factor) {
 
 
 template<class T>
-void calculateTranspose(T array[MATRIX_SIZE][MATRIX_SIZE], T factors[MATRIX_SIZE][MATRIX_SIZE], float r) {
-  T tempArray[MATRIX_SIZE][MATRIX_SIZE];
-  T inverseArray[MATRIX_SIZE][MATRIX_SIZE];
+void calculateTranspose(T matrix[MATRIX_SIZE][MATRIX_SIZE], T factors[MATRIX_SIZE][MATRIX_SIZE], float r) {
+  T tempMatrix[MATRIX_SIZE][MATRIX_SIZE];
+  T inverseMatrix[MATRIX_SIZE][MATRIX_SIZE];
   T d;
   for (int i = 0; i < r; i++) {
     for (int j = 0; j < r; j++) {
-      tempArray[i][j] = factors[j][i];
+      tempMatrix[i][j] = factors[j][i];
     }
   }
-  d = calculateDeterminant(array, r);
+  d = calculateDeterminant(matrix, r);
   for (int i = 0; i < r; i++) {
     for (int j = 0; j < r; j++) {
-      array[i][j] = tempArray[i][j] / d;
+      std::cout << "Matrix[i][j] = " << matrix[i][j] << " tempMatrix[i][j] = " << tempMatrix[i][j] << " d = " << d << std::endl;
+      matrix[i][j] = tempMatrix[i][j] / d;
     }
   }
+
+  // NOTE: chose to expand out the for loops rather than doing it in one loop
+  //       to improve performance in accessing memory where matrices stored
+
 }
 
 
@@ -143,6 +151,79 @@ void invertMatrixSerial(T matrix[][MATRIX_SIZE]) {
 }
 
 
+void betterMatrixInverse(int matrix[][MATRIX_SIZE]) {
+  for (int i = 0; i < MATRIX_SIZE; i++) {
+    for (int j = MATRIX_SIZE; j < 2 * MATRIX_SIZE; j++) {
+      if (i == (j - MATRIX_SIZE)) {
+        matrix[i][j] = 1;
+      } else {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+  
+  int temp; 
+  for (int i = 0; i < MATRIX_SIZE; i++) {
+    temp = matrix[i][i];
+    for (int j = 0; j < 2 * MATRIX_SIZE; j++) {
+      matrix[i][j] = matrix[i][j] / temp;
+    }
+  }
+}
+
+
+
+void gaussJordanInverse(int matrix[][MATRIX_SIZE]) {
+  float inverse[MATRIX_SIZE][MATRIX_SIZE];
+  for (int i = 0; i < MATRIX_SIZE; i++) {
+    for (int j = 0; j < MATRIX_SIZE; j++) {
+      inverse[i][j] = (float)matrix[i][j];
+    }
+  }
+  for (int i = 0; i < MATRIX_SIZE; i++) {
+    for (int j = 0; j < 2 * MATRIX_SIZE; j++) {
+      if (j == (i + MATRIX_SIZE)) {
+        inverse[i][j] = 1;
+      }
+    }
+  }
+  // partial pivoting
+  float d;
+  for (int i = MATRIX_SIZE; i > 1; i--) {
+    if (matrix[i - 1][1] < inverse[i][1]) {
+      for (int j = 0; j < MATRIX_SIZE * 2; j++) {
+        d = inverse[i][j];
+        inverse[i][j] = inverse[i-1][j];
+        inverse[i-1][j] = d;
+      }
+    }
+  }
+  // reducing to diagonal matrix:
+  for (int i = 0; i < MATRIX_SIZE; i++) {
+    for (int j = 0; j < MATRIX_SIZE; j++) {
+      if (j != i) {
+        d = inverse[j][i] / inverse[i][i];
+        for (int k = 1; k < MATRIX_SIZE * 2; k++) {
+          inverse[j][k]-= inverse[i][k] * d;
+        }
+      }
+    }
+  }
+  // reducing to unit matrix
+  for (int i = 0; i < MATRIX_SIZE; i++) {
+    d = inverse[i][i];
+    for (int j = 0; j < MATRIX_SIZE * 2; j++) {
+      inverse[i][j] = inverse[i][j] / d;
+    }
+  }
+
+  printMatrix(inverse);
+
+  return;
+}
+
+
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     std::cerr << "Args wrong. Usage: ./matrix-inversion [shouldPrint]" << std::endl;
@@ -156,11 +237,7 @@ int main(int argc, char **argv) {
   if (shouldPrint) {
     printMatrix(matrix);
   }
-  invertMatrixSerial(matrix);
-  if (shouldPrint) {
-    std::cout << "\n\n";
-    printMatrix(matrix);
-  }
+  gaussJordanInverse(matrix);
   return 0;
 }
 
