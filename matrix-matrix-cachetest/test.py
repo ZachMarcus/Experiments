3b0afb0827294673b006b237a8a12e0e4af652e7
Zachary Marcus <zmarcus@Theta.coe.neu.edu>
2017-11-28 16:47:35 -0500

    Fixed up full test script and added some results

--- matrix-matrix-cachetest/test.py
+++ matrix-matrix-cachetest/test.py
@@ -7,53 +7,49 @@ import json
 
 def runTest(matrix, tile):
     process = subprocess.check_output(["./matrix-matrix", str(matrix), str(tile)])
-    return float(process.replace("\n",""))
+    return 1000000 * float(process.replace("\n","")) / (matrix ** 3)
 
 
-def runTests(first_bound, second_bound, test_file):
+def runTests(matrix_lower_bound, matrix_upper_bound, tile_lower, tile_upper, test_file):
     results = {}
-    matrix_sizes = [x * 4 for x in range(first_bound, second_bound)]
+    matrix_sizes = [x for x in range(matrix_lower_bound, matrix_upper_bound, 4)]
     for matrix in matrix_sizes:
         results[matrix] = []
-    tile_sizes = []
-    n = 1
-    while n < first_bound:
-        tile_sizes.append(n)
-        n += 2
+    tile_sizes = [x for x in range(tile_lower, tile_upper, 2)]
+
     for matrix in matrix_sizes:
         for tile in tile_sizes:
             results[matrix].append(runTest(matrix, tile))
 
-    with open(test_file, "w") as result_file:
+    with open(test_file, 'a+') as result_file:
         all_matrices = sorted(results.keys())
-        result_file.write("Tile,")
+        result_file.write('Tile,')
         for matrix in all_matrices:
             result_file.write(str(matrix))
             if matrix != all_matrices[-1]:
-                result_file.write(",")
-        result_file.write("\n")
+                result_file.write(',')
+        result_file.write('\n')
 
         for tile in range(len(tile_sizes)):
-            result_file.write(str(tile_sizes[tile]) + str(","))
+            result_file.write(str(tile_sizes[tile]) + str(','))
             for matrix in all_matrices:
                 result_file.write(str(results[matrix][tile]))
                 if matrix != all_matrices[-1]:
-                    result_file.write(",")
-            result_file.write("\n")
-
+                    result_file.write(',')
+            result_file.write('\n')
 
 
 def makeCheck():
-    if not os.path.exists("./matrix-matrix"):
-        process = subprocess.check_output(["make"])
-        if process.count("\n") != 1:
+    if not os.path.exists('./matrix-matrix'):
+        process = subprocess.check_output(['make'])
+        if process.count('\n') != 1:
             print "Error in build step"
             exit(1)
 
 
 if __name__ == "__main__":
     makeCheck()
-    runTests(40, 60, "output.csv")
+    runTests(20, 100, 1, 20, "results.csv")
 
 
 
