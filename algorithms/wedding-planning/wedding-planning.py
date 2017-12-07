8f14007425dfd3816437e1a7d99124bf785e2602
Zachary Marcus <zmarcus@Iota.coe.neu.edu>
2017-12-07 12:09:50 -0500

    Removed useless files and cleanup up dir

--- algorithms/wedding-planning/wedding-planning.py
+++ algorithms/wedding-planning/wedding-planning.py
@@ -3,14 +3,10 @@
 import sys
 
 
-
 class City:
     def __init__(self, number, weight):
         self.number = number
         self.weight = weight
-        self.outgoing_neighbors = {} # keys are city nums, vals are weights
-        self.incoming_neighbors = {} # keys are city nums, vals are weights
-        self.routes_left = 50
 
     def __eq__(self, rhs):
         return not self.weight < rhs.weight and not rhs.weight < self.weight
@@ -25,89 +21,106 @@ class City:
     def __le__(self, rhs):
         return not rhs.weight < self.weight
 
-    def __str__(self):
-        ret = ''
-        ret += 'City Number: ' + str(self.number) + ' Weight: ' + str(self.weight)
-        ret += '\nOutgoing(#,Weight): '
-        for key, val in self.outgoing_neighbors.items():
-            ret += '(' + str(key) + ',' + str(val) + ');'
-        ret += '\nIncoming(#,Weight): '
-        for key, val in self.incoming_neighbors.items():
-            ret += '(' + str(key) + ',' + str(val) + ');'
-        return ret
 
-
-# Implements classic heap functionality in that you can get the smallest items
-# first, but also has good access times as a python dictionary
-class Heap_Map(dict):
+class Min_Heap:
     def __init__(self):
-        self.min_heap = []
-        dict.__init__(self)
+        self.length = 0
+        self.cities = []
+        self.cities_mapping = {}
+
+    def swap(self, left_index, right_index):
+        # swap values
+        temp_weight = self.cities[left_index].weight
+        temp_number = self.cities[left_index].number
+        self.cities[left_index].weight = self.cities[right_index].weight
+        self.cities[left_index].number = self.cities[right_index].number
+        self.cities[right_index].weight = temp_weight
+        self.cities[right_index].number = temp_number
+        # Swap the keys
+        number_of_left = self.cities[left_index].number
+        number_of_right = self.cities[right_index].number
+        temp_mapping = self.cities_mapping[number_of_left]
+        self.cities_mapping[number_of_left] = self.cities_mapping[number_of_right]
+        self.cities_mapping[number_of_right] = temp_mapping
+        return
+
+
+    def update(self, city_number, new_weight):
+        index = self.cities_mapping[city_number]
+        self.cities[index].weight = new_weight
+        self.bubble_up(index)
+        return
+        for index in range(0, len(self.cities)):
+            if self.cities[index].number == city_number:
+                self.cities[index].weight = new_weight
+                self.bubble_up(index)
+                return
+        
+    def insert(self, city):
+        self.cities.append(city)
+        self.cities_mapping[city.number] = self.length
+        self.bubble_up(self.length)
+        self.length += 1
+        return
+
+    def top(self):
+        if self.length == 0:
+            return
+        return self.cities[0]
 
     def pop(self):
-        if len(self) == 0:
-            print('Heap_dictionary is empty')
-#        temp_heap = self.min_heap
-        while self.min_heap[0][1] not in self or self[self.min_heap[0][1]] != self.min_heap[0][0]:
-            last_heap_item = self.min_heap.pop()
-            where_to_put_it = 0
-            running = True
-            while running:
-                child_index = 2 * where_to_put_it + 1
-                if child_index + 1 < len(self.min_heap) and self.min_heap[child_index] > self.min_heap[child_index + 1]:
-                    child_index += 1
-                if child_index >= len(self.min_heap) or last_heap_item <= self.min_heap[child_index]:
-                    self.min_heap[where_to_put_it] = last_heap_item
-                    running = False
-                if running:
-                    self.min_heap[where_to_put_it] = self.min_heap[child_index]
-                    where_to_put_it = child_index
-        return self.min_heap[0][0]
+        if self.length == 0:
+            return
+        self.length -= 1
+        temp_city = self.cities[0]
+        del self.cities_mapping[temp_city.number]
+        self.cities[0] = self.cities[self.length]
+        self.cities_mapping[self.cities[0].number] = 0
+        self.cities = self.cities[:-1]
+        self.bubble_down(0)
+        return
 
-    def __iter__(self):
-        '''
-        Destroy elements as it iterates
-        For use in main Dijkstra algorithm
-        '''
-        def iteration_lambda():
-            while len(self) > 0:
-                temp = self.pop()
-                yield temp
-                del self[temp.number]
-        return iteration_lambda()
+    def is_empty(self):
+        return self.length == 0
 
-    def __setitem__(self, key, val):
-        dict.__setitem__(self, key, val)
-        temp_heap = self.min_heap
-        if len(temp_heap) > 2 * len(self):
-            self.min_heap = [(value, key) for key, value in self.items()]
-            self.min_heap.sort()
-        else:
-            temp = (val, key)
-            where_to_put_it = len(temp_heap)
-            temp_heap.append(None)
-            while where_to_put_it > 0 and temp < temp_heap[(where_to_put_it - 1) // 2]:
-                temp_heap[where_to_put_it] = temp_heap[(where_to_put_it - 1) // 2]
-                where_to_put_it = (where_to_put_it - 1) // 2
-            temp_heap[where_to_put_it] = temp
+    def heapify(self):
+        for i in range(self.length - 1, -1, -1):
+            self.bubble_down(i)
 
-    def setdefault(self, key, val):
-        '''
-        A new version of setdefault
-        '''
-        if key not in self:
-            self[key] = val
-        return self[key]
+    def bubble_down(self, vertex):
+        left_child_ind = 2 * vertex + 1
+        right_child_ind = 2 * vertex + 2
+        min_vertex = vertex
+        # Check if we're at a leaf
+        if left_child_ind >= self.length:
+            return
+        if self.cities[vertex].weight > self.cities[left_child_ind].weight:
+            min_vertex = left_child_ind
+        if right_child_ind < self.length and self.cities[min_vertex].weight > self.cities[right_child_ind].weight:
+            min_vertex = right_child_ind
+        if min_vertex != vertex:
+            self.swap(min_vertex, vertex)
+            self.bubble_down(min_vertex)
+        return
+
+    def bubble_up(self, vertex):
+        if vertex == 0:
+            return
+        max_vertex = int((vertex - 1) / 2)
+        if self.cities[max_vertex].weight > self.cities[vertex].weight:
+            self.swap(max_vertex, vertex)
+            self.bubble_up(max_vertex)
+        return
 
 
-class Wedding_Search:
+class Wedding_Planner:
     def __init__(self):
-        self.heap = Heap_Map()
-        self.distances = []
-        self.predecessors = []
-        self.visited_cities = []
+        self.visited_cities = {}
+        self.distances = {}
+        self.outgoing_vertices = {}
+        self.min_heap = Min_Heap()
 
-    def start(self):
+    def plan(self):
         num_cities = -1
         num_edges = -1
         root_city = -1
@@ -115,82 +128,51 @@ class Wedding_Search:
         num_cities = int(first_line[0])
         num_edges = int(first_line[1])
         root_city = int(first_line[2])
+        
+        for vertex in range(1, num_cities+1):
+            self.outgoing_vertices[vertex] = {}
+
         for x in range(0, num_edges):
             # src, dest, weight
             temp = sys.stdin.readline().rstrip().split(' ') # U, V, D
             src_city = int(temp[0])
             dst_city = int(temp[1])
-            path_weight = int(temp[2])
-            if src_city not in self.heap:
-                city = City(src_city, float('inf'))
-                city.outgoing_neighbors[dst_city] = path_weight
-                self.heap[src_city] = city
-            else: # Add value to outgoing neighbors adjacency list
-                city = self.heap[src_city]
-                city.outgoing_neighbors[dst_city] = path_weight
-                self.heap[src_city] = city
-            if dst_city not in self.heap:
-                city = City(dst_city, float('inf'))
-                city.incoming_neighbors[src_city] = path_weight
-                self.heap[dst_city] = city
-            else: # Add value to incoming neighbors adjacency list
-                city = self.heap[dst_city]
-                city.incoming_neighbors[src_city] = path_weight
-                self.heap[dst_city] = city
-        # Now fix the case where some vertices just don't do anything
-        for x in range(1, num_cities + 1):
-            if x not in self.heap:
-                self.heap[x] = City(x, float('inf'))
-# FIX THIS NOW
+            path_weight = int(temp[2])        
+            self.outgoing_vertices[dst_city][src_city] = path_weight
 
-        # Now fix the root city:
-        root = self.heap[root_city]
-        root.weight = 0
-        self.heap[root_city] = root
+        # intialization
+        for city in range(1, num_cities + 1):
+            self.visited_cities[city] = False
+            self.distances[city] = float('inf')
+            self.min_heap.insert(City(city, float('inf')))
 
-        self.distances.append(None) # The cities are 1-indexed
-        self.predecessors.append(None) # The predecessors are 1-indexed
-        for city in self.heap.keys():
-            self.distances.append(float('inf'))
-            self.predecessors.append(None)
-        # Now fix the root city
         self.distances[root_city] = 0
+        self.min_heap.update(root_city, 0)
 
-#        print('Distances: {}\nPredecessors: {}'.format(self.distances, self.predecessors))
-        for city in self.heap:
-#            print('Visiting city: {}'.format(city))
-            self.visited_cities.append(city) # S := S Union with new City
-            for incoming, weight in city.incoming_neighbors.items():
-                curr_dist = self.distances[incoming]
-                other_dist = self.distances[city.number] + weight
-                
-                if curr_dist > other_dist:
-                    self.distances[incoming] = other_dist
-                    self.predecessors[incoming] = city.number
-                    # Now we need to update the Heap
-                    if incoming in self.heap:
-                        self.heap[incoming].weight = other_dist
-#                    else:
-#                        print('{} Not in heap anymore'.format(incoming))
-#            print('Distances: {}\nPredecessors: {}'.format(self.distances, self.predecessors))
-            
-#        print('Distances: {}\nPredecessors: {}'.format(self.distances, self.predecessors))
-        to_print = ''
-        for x in range(1, len(self.distances)):
-            if self.distances[x] != float('inf'):
-                to_print += str(self.distances[x]) + ' '
+        while not self.min_heap.is_empty():
+            city = self.min_heap.top()
+            self.min_heap.pop()
+            self.visited_cities[city.number] = True
+            for vertex, weight in self.outgoing_vertices[city.number].items():
+                new_dist = self.distances[city.number] + weight
+                old_dist = self.distances[vertex]
+                if new_dist < old_dist:
+                    self.distances[vertex] = new_dist
+                    self.min_heap.update(vertex, new_dist)
+
+        ret = ''
+        for city in range(1, num_cities+1):
+            if self.distances[city] == float('inf'):
+                ret += '-1 '
             else:
-                to_print += '-1 '
-        to_print = to_print[:-1]
-        print(to_print)
+                ret += str(self.distances[city]) + ' '
+        print(ret[:-1])
+
+
+
 
 
 if __name__ == '__main__':
-    wedding_search = Wedding_Search()
-    wedding_search.start()
-
-
-
-
-
+    wedding_planner = Wedding_Planner()
+    wedding_planner.plan()
 
